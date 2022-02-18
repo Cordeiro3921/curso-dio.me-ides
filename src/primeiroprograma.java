6b73f128ec6281fdd73bbf74070939c75c7f3e90
Danilo codeiro <daniloroot6@gmail.com>
2022-02-18 17:32:57 -0300

    first commit

---
+++ src/primeiroprograma.java
@@ -0,0 +1,19 @@
+import com.company.Gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato ();
+
+
+        System.out.println(gato);
+
+
+
+        /*int a = 5 ;
+        ins b = 3 ;
+
+        System.out.println("hello word");*/
+    }
+
+}
